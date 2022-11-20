857553c05ed591ed85df97d768b1c844ef2dea41
SalihHakanUlufer <hakanulufer@hotmail.com>
2022-11-20 19:10:00 +0100

    bookmarks && css begining

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,13 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+# def create
+
+
+# end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "lists#index"
-  resources :lists, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :lists, only: [:index, :show, :new, :create, :edit, :update]
 
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
+
+
+  # get 'lists/:list_id/bookmark/new', to: 'bookmark#new', as: 'new_ bookmark'
+  # post 'lists/:list_id/bookmark', to: 'bookmark#create', as: 'bookmark'
 end
